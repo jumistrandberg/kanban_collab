a45a6e0b147818859120e5d00f2dffaae207f6aa
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-04-16 13:44:05 +0200

    Installed react-router-dom, created the pages included in routing and fixed routing

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+
+//Components
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+
+//Pages
 import Board from "../pages/Board";
+import Admin from "../pages/Admin";
+import MissingPage from "../pages/MissingPage";
+import Settings from "../pages/Settings";
+// import List from "../pages/List";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Board />
+        <Routes>
+          <Route path="/" element={<Board />} />
+          {/* TODO - List är utkommenterat till Kuiper har skapat sidan */}
+          {/* <Route path="/list" element={<List />}/> */}
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="*" element={<MissingPage />} />
+        </Routes>
       </main>
       <Footer />
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import App from "./app/App.jsx";
 import "./index.css";
 import store from "./app/store.js";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />}></Route>
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>
 );
