febb9ef8cd41910064ef1791f75d19be62ed6798
Anna-Sara Isakson <annasaraisakson@hotmail.com>
2024-04-19 17:05:31 +0200

    Fix drag and drop inside and between columns. Put start columns in local storage

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -4,20 +4,28 @@ import { useDispatch } from "react-redux";
 import { removeTask } from "../features/tasks/taskSlice";
 import { RxAvatar } from "react-icons/rx";
 import { MdOutlineDeleteForever as DeleteBtn } from "react-icons/md";
+import DropIndicator from "./DropIndicator";
 
-const Task = ({ task }) => {
+const Task = ({ columnId, handleDragStart, task }) => {
   const dispatch = useDispatch();
   return (
-    <div className={styles.taskContainer}>
-      <h4>{task.title}</h4>
-      <RxAvatar />
+    <>
+      <DropIndicator beforeTaskId={task.id} columnId={columnId} />
+      <div
+        className={styles.taskContainer}
+        draggable={true}
+        onDragStart={(e) => handleDragStart(e, task)}
+      >
+        <h4>{task.title}</h4>
+        <RxAvatar />
 
-      <p>{new Date().toDateString("sv-SV")}</p>
-      <DeleteBtn
-        className={styles.deleteIcon}
-        onClick={() => dispatch(removeTask(task.id))}
-      />
-    </div>
+        <p>{new Date().toDateString("sv-SV")}</p>
+        <DeleteBtn
+          className={styles.deleteIcon}
+          onClick={() => dispatch(removeTask(task.id))}
+        />
+      </div>
+    </>
   );
 };
 
