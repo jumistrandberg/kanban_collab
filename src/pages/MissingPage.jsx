bd81bc888c1cd3dbfe01cf5782f29cb34052f9ea
Johan Söderlund <johan.soderlund96@gmail.com>
2024-04-25 19:03:42 +0200

    created missing page with styling, img and redirect back to home

--- src/pages/MissingPage.jsx
+++ src/pages/MissingPage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../styling/MissingPage.module.css";
 
 const MissingPage = () => {
-  return <div>MissingPage</div>;
+  return (
+    <div className={styles.MissingContainer}>
+      <p className={styles.MissingText}>
+        Oops. We couldn't find the page you are looking for...
+      </p>
+      <Link to="/" className={styles.MissingBtn}>
+        Go Back Home
+      </Link>
+    </div>
+  );
 };
 
 export default MissingPage;
