a1773734de89dc2a95f3e09f2f848dbe6173d569
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2024-05-01 20:29:46 +0200

    user active is done

--- src/features/users/UsersList.jsx
+++ src/features/users/UsersList.jsx
@@ -1,17 +1,34 @@
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import styles from "../../styling/UsersList.module.css"
 import { FcReadingEbook } from "react-icons/fc";
+import { FcPortraitMode } from "react-icons/fc";
+import { changeActiveUser } from "../users/usersSlice";
 
 
 
 
 const UsersList = () => {
 
+    const dispatch = useDispatch();
+
+
     const users = useSelector((state) => state.allUsersReducer.users);
-    console.log(users)
     const renderUsers = users.map(user => (
         <div key={user.id} className={styles.user}>
-            <FcReadingEbook role="button" className={styles.userAvatar} style={{'background-color': user.userAvatarColor}}/>
+
+            {!user.userActive && <FcReadingEbook 
+                role="button" 
+                onClick={() => dispatch(changeActiveUser({id: user.id}))} 
+                className={styles.userAvatar} 
+                style={{'backgroundColor': user.userAvatarColor}}
+            />}
+            {user.userActive && <FcPortraitMode 
+                role="button" 
+                onClick={() => dispatch(changeActiveUser({id: user.id}))} 
+                className={styles.userAvatar} 
+                style={{'backgroundColor': user.userAvatarColor}}
+            />}
             <p className={styles.userTitle}>{user.userUserName}</p>
         </div>
     ))
